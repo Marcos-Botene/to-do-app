e90d300f99e3e99100bb752d5322d2dd03045e2a
Marcos_Botene <marcos.botene@leaderweb.com.br>
2021-07-22 18:05:31 -0300

    removed: old components.

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Header } from './header';
+export { ItemWrapper } from './item-wrapper';
+export { TasksList } from './tasks-list';
+export type { Task } from './tasks-list';
+export { TodoInput } from './todo-input';
