a63e0d352ed0320347d03f92bc07e2d8db671849
Marcos_Botene <marcos.botene@leaderweb.com.br>
2021-07-23 14:13:36 -0300

    created: marking and unmarking a task as completed.

--- src/components/tasks-list/index.tsx
+++ src/components/tasks-list/index.tsx
@@ -39,22 +39,16 @@ export function TasksList({
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
                 onPress={() => toggleTaskDone(item.id)}
               >
                 <View
                   testID={`marker-${index}`}
-                  style={[
-                    item.done ? styles.taskMarkerDone : styles.taskMarker,
-                  ]}
+                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
                 >
                   {item.done && <Icon name='check' size={12} color='#FFF' />}
                 </View>
 
-                <Text
-                  //TODO - use style prop
-                  style={[item.done ? styles.taskTextDone : styles.taskText]}
-                >
+                <Text style={item.done ? styles.taskTextDone : styles.taskText}>
                   {item.title}
                 </Text>
               </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,27 +6,27 @@ import { Header, TodoInput, TasksList, Task } from '../components';
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    setTasks(oldState => [
-      ...oldState,
-      {
-        id: new Date().getTime(),
-        title: newTaskTitle,
-        done: false,
-      },
-    ]);
-  }
+  const handleAddTask = (newTaskTitle: string) => {
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
 
-  function handleToggleTaskDone(id: number) {
-    setTasks(oldState =>
-      oldState.filter(task => (task.id === id ? !task.done : false))
+    setTasks(oldState => [...oldState, newTask]);
+  };
+
+  const handleToggleTaskDone = (id: number) => {
+    const updatedTask = tasks.map(task =>
+      task.id === id ? { ...task, done: !task.done } : task
     );
-  }
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(updatedTask);
+  };
+
+  const handleRemoveTask = (id: number) => {
     setTasks(oldState => oldState.filter(task => task.id !== id));
-  }
+  };
 
   return (
     <View style={styles.container}>
